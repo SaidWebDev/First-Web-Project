710a2a551cd59f5a29c80c1f69c562d7c8f2eeff
said moutaa <smoutaa@gmail.com>
2019-07-25 22:53:40 +0200

    Make sure the response page returns an html page

--- node/index.js
+++ node/index.js
@@ -1,14 +1,16 @@
 const http = require('http');
 const path = require('path');
-const fs = require('fs');
+const fs = require ('fs');
 
-const server = http.createServer((req,res)=>{
-if(req.url==='/')
-    res.end('<h1>HOME</h1>');
-
+const server= http.createServer((req, res)=>
+{
+    if(req.url==='/'){
+        res.writeHead(200,{'Content-Type': 'Text/html'});
+        res.end('<h1>HOME<h1>');
+    }
 }
 );
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () =>console.log(`server running on port ${PORT}` ));
+server.listen(PORT, ()=> console.log(`Server is running on ${PORT}`));
