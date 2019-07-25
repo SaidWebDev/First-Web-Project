70f572a255ce584ecd81a42a77e51e49e3f89cda
said moutaa <smoutaa@gmail.com>
2019-07-25 19:38:43 +0200

    update index.js and add logger.ts

--- node/index.js
+++ node/index.js
@@ -1,6 +1,11 @@
-const Person = require('./person');
+const Logger = require('./logger.ts');
+const logger = new Logger();
 
+logger.on('message', data => console.log(data));
+logger.log('Howdiiiiiie!');
+
+
+
+/* const Person = require('./person');
 const p = new Person('Jacke', 24);
-
-
-p.greeting();
+p.greeting(); */
