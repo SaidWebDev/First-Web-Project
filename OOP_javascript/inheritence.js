db881b5422aee27861f4a75aae502f312b87a053
said moutaa <smoutaa@gmail.com>
2019-07-28 12:48:21 +0200

    Prototypes inheritence

--- OOP_javascript/inheritence.js
+++ OOP_javascript/inheritence.js
@@ -1,54 +1,32 @@
-//Car class declaration
-
-/* class Car{
-    constructor(){
-        this.manufcaturer="Toyota";
-        this.model="Yaris";
-        this.year="2000";
-    }
-
-    carmodel(){
-        console.log(`This is a ${this.manufcaturer} ${this.model}`);
-    }
-
-    set setcarmodel(newmodel){
-        this.model=newmodel;
-    }
-    set setcarmanufacturer(newmanufacturer){
-        this.model=newmanufacturer;
-    }
-    set setcaryear(year){
-        this.year=year;
-    }
-}
-
-Car.prototype.yearOfRelease = function(){
-    console.log(`This car was released in ${this.year}`);
-}
-
-Car.prototype.yearOfRelease = function(){
-    console.log(`This car was released in ${this.year}`);
-}
- */
-//SUV constructor
+//Car constructor
 
 function Car(manufacturer,model, year){
     this.manufacturer=manufacturer;
     this.model=model;
     this.year=year;
 }
+Car.prototype.getSummary= function(){
+return `This is a ${this.manufacturer} ${this.model} made in ${this.year}`;
+}
 
-function suv(manufacturer,model, year, offroad){
+//Suv constructor
+function Suv(manufacturer,model, year, offroad){
 
     Car.call(this,manufacturer,model,year, offroad);
    // this.yearOfRelease= year;
     this.offroad="yes";
-
 }
 
-const JeepCherocke= new suv("Jeep", "Cherocke", "2019", false);
+const toyotaYaris = new Car("Toyota", "yaris", "2015");
+console.log(Object.values(toyotaYaris));
+console.log(toyotaYaris.getSummary()); 
 
-console.log(Object.values(JeepCherocke));
+//Inherit prototype
+Suv.prototype=Object.create(Car.prototype);
 
+// Instantiate an suv
+const jeepCherocke= new Suv("Jeep", "Cherocke", "2019", false);
+console.log(Object.values(jeepCherocke));
+console.log(jeepCherocke.getSummary());
 
 
