885f3302a437192aacd7c3820e093e324bf37844
said moutaa <smoutaa@gmail.com>
2019-08-04 19:06:25 +0200

    second commit

--- ES6-coding-the-bleeding-edge/Destructiong-objects.js
+++ ES6-coding-the-bleeding-edge/Destructiong-objects.js
@@ -9,3 +9,30 @@ const {firstname:fn, lastname:ln } = personalInformation;
 
 console.log(fn, ln );
 console.log(`the full name is : ${fn}    ${ln}`)
+
+//The functions addressMaker1 and addressmaker2 are equivalent
+
+function addressMaker1(address) {
+     const newAddress ={
+          city: address.city,
+          state: address.state,
+          country: 'France'
+     };
+     console.log(newAddress);
+}
+
+addressMaker1({city:'Paris', state: 'Ile de France'});
+
+function addressmaker2(address){
+
+     const {city,state}= address;
+
+     const newAddress ={
+          city,
+          state,
+          country:'France'
+     }
+     console.log(newAddress);
+}
+
+addressmaker2({city:'Nice', state:`Alpes Maritimes`});
